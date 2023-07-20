9652bf16d1d566d1cf80e783a23aab73abf178a4
Craluminum2413 <beware.14b@gmail.com>
2023-07-20 21:01:51 +0300

    inherit from ChestPatch

--- src/Util/Patches/DoubleChestPatch.cs
+++ src/Util/Patches/DoubleChestPatch.cs
@@ -3,34 +3,12 @@ using System.Linq;
 
 namespace WoodChests;
 
-public class DoubleChestPatch
+public class DoubleChestPatch : ChestPatch
 {
-    public List<string> Types { get; set; }
-    public string DefaultType { get; set; }
-    public Dictionary<string, string> RotatatableInterval { get; set; }
-    public Dictionary<string, bool> Drop { get; set; }
-    public Dictionary<string, int> QuantitySlots { get; set; }
-    public Dictionary<string, string> DialogTitleLangCode { get; set; }
-    public Dictionary<string, int> StorageType { get; set; }
-    public Dictionary<string, bool> RetrieveOnly { get; set; }
-    public Dictionary<string, string> Shape { get; set; }
-    public Dictionary<string, string> TypedOpenSound { get; set; }
-    public Dictionary<string, string> TypedCloseSound { get; set; }
-
     public Dictionary<string, int> QuantityColumns { get; set; }
 
-    public DoubleChestPatch(List<string> types)
+    public DoubleChestPatch(List<string> types) : base(types)
     {
-        Types = types;
-        DefaultType = types[0];
-
-        Drop = types.ToDictionary(key => key, _ => true);
-        RetrieveOnly = types.ToDictionary(key => key, _ => false);
-        RotatatableInterval = types.ToDictionary(key => key, _ => "22.5degnot45deg");
-        StorageType = types.ToDictionary(key => key, _ => 189);
-        TypedCloseSound = types.ToDictionary(key => key, _ => "game:sounds/block/largechestclose");
-        TypedOpenSound = types.ToDictionary(key => key, _ => "game:sounds/block/largechestopen");
-
         DialogTitleLangCode = types.ToDictionary(key => key, _ => "trunkcontents");
         QuantitySlots = types.ToDictionary(key => key, _ => 36);
         Shape = types.ToDictionary(key => key, _ => "game:block/wood/trunk/normal");
